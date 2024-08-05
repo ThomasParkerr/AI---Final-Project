27335bafacd6413d16cac2c2e42be203e3c55c27
ThomasParkerr <145340119+ThomasParkerr@users.noreply.github.com>
2024-08-05 05:25:11 +0000

    App for hosting our website online

--- onlineapp.py
+++ onlineapp.py
@@ -150,15 +150,3 @@ def main():
 if __name__ == "__main__":
     main()
 
-import subprocess
-from pyngrok import ngrok
-
-# Use your ngrok auth token
-ngrok.set_auth_token("2jUZ8Jy50WpJ3iJOHm1xnxLYni8_5dZK7zB5pudZAdeVtg3Dc")
-
-# Run Streamlit app
-process = subprocess.Popen(['streamlit', 'run', 'onlineapp.py'])
-
-# Expose the Streamlit app via ngrok
-public_url = ngrok.connect(addr="8501", proto="http")
-print(f'Streamlit app is live at {public_url}')
