c37b3486bec954769c8734e15e016a0c7729e966
ThomasParkerr <145340119+ThomasParkerr@users.noreply.github.com>
2024-08-05 06:15:39 +0000

    For testing functionality before Deployment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import cv2
 from utils import read_video, save_video
 from trackers import Tracker
-import cv2
 import numpy as np
 from team_assigner import TeamAssigner
 from player_ball_assigner import PlayerBallAssigner
