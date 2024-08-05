0fcc4957bd7a93c86fc6ab6ec1f385bf3716fc5e
ThomasParkerr <145340119+ThomasParkerr@users.noreply.github.com>
2024-08-05 07:28:14 +0000

    Used to deploy online app on the internet

--- deploy.py
+++ deploy.py
@@ -1,11 +1,15 @@
 import subprocess
 from pyngrok import ngrok
+import time
 
 # Use your ngrok auth token
 ngrok.set_auth_token("2jUZ8Jy50WpJ3iJOHm1xnxLYni8_5dZK7zB5pudZAdeVtg3Dc")
 
 # Run Streamlit app
-process = subprocess.Popen(['streamlit', 'run', 'onlineapp.py'])
+process = subprocess.Popen(['streamlit', 'run', 'app.py'])
+
+# Give the server some time to start
+time.sleep(5)
 
 # Expose the Streamlit app via ngrok
 public_url = ngrok.connect(addr="8501", proto="http")
